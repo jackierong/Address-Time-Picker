2d08f345880286b7f7e3e034c995a78be032ef20
Rong <rong@rongdemac-mini.local>
2017-01-13 19:27:30 +0800

    Initial Commit

---
+++ PickerTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PickerTest
+//
+//  Created by Rong on 17/1/13.
+//  Copyright © 2017年 Rong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PickerTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PickerTest
+//
+//  Created by Rong on 17/1/13.
+//  Copyright © 2017年 Rong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
